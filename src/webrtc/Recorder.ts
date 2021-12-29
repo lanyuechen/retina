13558802543626100595cb039c46416b47c9c0fe
zhenglipeng <zhenglipeng@sensetime.com>
2021-12-29 16:38:06 +0800

    添加recorder功能

---
+++ src/webrtc/Recorder.ts
@@ -0,0 +1,47 @@
+
+export default class {
+  mediaRecorder: MediaRecorder;
+  chunks: Blob[] = [];
+
+  // eslint-disable-next-line no-undef
+  constructor(stream: MediaStream, options?: MediaRecorderOptions) {
+    this.mediaRecorder = new MediaRecorder(stream, options);
+
+    this.mediaRecorder.ondataavailable = this.handleDataAvailable.bind(this);
+  }
+
+  start() {
+    this.mediaRecorder.start();
+  }
+
+  stop() {
+    this.mediaRecorder.stop();
+  }
+
+  pause() {
+    this.mediaRecorder.pause();
+  }
+
+  resume() {
+    this.mediaRecorder.resume();
+  }
+
+  handleDataAvailable(event: BlobEvent) {
+    if (event.data.size > 0) {
+      this.chunks.push(event.data);
+    }
+  }
+
+  download() {
+    const blob = new Blob(this.chunks, { type: 'video/mp4' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.style.display = 'none';
+    a.href = url;
+    a.download = `${new Date().toISOString()}.mp4`;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+  }
+}
