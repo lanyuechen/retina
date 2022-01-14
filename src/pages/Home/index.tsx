566f4c9295493e8c1d2824a259ff1f1dc6d37602
zhenglipeng <zhenglipeng@sensetime.com>
2022-01-14 18:51:45 +0800

    首页描述修改

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -12,7 +12,7 @@ export default () => {
           <img src={`${import.meta.env.BASE_URL}favicon.svg`} alt="retina" />
           &nbsp;Retina
         </h1>
-        <h2>Retina is a simple but powerful tool for designers and developers.</h2>
+        <h2>Retina is a simple but powerful tool for video conference.</h2>
         <a href="https://github.com/lanyuechen/retina" className={style.btn}>
           <Icon type="github" />&nbsp;&nbsp;Github
         </a>
